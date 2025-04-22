3eb443f7c26ad7f1448d45674f7eedb3fc501520
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-22 15:08:52 +0200

    [UPD] Add post controller

---
+++ controllers/postController.js
@@ -0,0 +1,34 @@
+const posts = require('../app.js')
+
+
+//index
+function index(req, res){
+
+}
+
+//show
+function show(req, res){
+
+}
+
+//store
+function store(req, res){
+
+}
+
+//update
+function update(req, res){
+
+}
+
+//modify
+function modify(req, res){
+
+}
+
+//destroy
+function destroy(req, res){
+
+}
+
+module.exports = {index, show, store, update, modify, destroy}
